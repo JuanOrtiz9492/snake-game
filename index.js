e9260bb0cd5876e28bd5756c87c2b15bad642306
Juan Ortiz <ortizjuandavid@gmail.com>
2022-11-27 23:10:13 -0500

    snake grows properly, also apples appear properly

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const GO_RIGHT = "GO_RIGHT";
 const GO_LEFT = "GO_LEFT";
 const GO_UP = "GO_UP";
 const GO_DOWN = "GO_DOWN";
+const CANVAS_WIDTH = 600;
+const CANVAS_HEIGHT = 600;
+const SIZE = 10;
 
 class SnakeBody {
-  constructor({ width, height, posX = 50, posY = 50 }) {
-    this.width = width;
-    this.height = height;
+  constructor({ size, posX = 50, posY = 50 }) {
+    this.width = size;
+    this.height = size;
     this._posX = posX;
     this._posY = posY;
     this._prevX = undefined;
@@ -28,6 +31,7 @@ class SnakeBody {
 
   draw() {
     fill(51);
+    stroke(255);
     rect(this._posX, this._posY, this.width, this.height);
   }
 }
@@ -38,13 +42,24 @@ class Snake extends Array {
       return Snake.instace;
     }
     Snake.instace = this;
-    this.push(new SnakeBody({ width: 10, height: 10 }));
+    this.push(new SnakeBody({ size: SIZE }));
     return this;
   }
 
-  appendBody() {
-    const [nextPosX, nextPosY] = this[this.length - 1].getPrevPos();
-    this.unshift(new Snake({ posX: nextPosX, posY: nextPosY }));
+  hasHitApple(apple) {
+    const head = this[this.length - 1];
+    const [posX, posY] = head.getCurrentPos();
+    const [applePosX, applePosY] = apple.getCurrentPos();
+    if (Math.abs(posX - applePosX) < 10 && Math.abs(posY - applePosY) < 10) {
+      apple.generate();
+      const tail = this[0];
+      const [nextPosX, nextPosY] = tail.getCurrentPos();
+      this.appendBody(nextPosX, nextPosY);
+    }
+  }
+
+  appendBody(posX, posY) {
+    this.unshift(new SnakeBody({ size: SIZE, posX, posY }));
   }
 
   moveAnimal(motion) {
@@ -60,7 +75,6 @@ class Snake extends Array {
       nextPosX = a;
       nextPosY = b;
     });
-    this.draw();
   }
 
   draw() {
@@ -75,6 +89,10 @@ class Motion {
     this.currentDirection = GO_RIGHT;
     this.nextDirection = undefined;
   }
+
+  getCurrentDirection() {
+    return this.currentDirection;
+  }
   changeDirection(nextDirection) {
     if (
       [GO_LEFT, GO_RIGHT, GO_DOWN, GO_UP].some(
@@ -86,10 +104,10 @@ class Motion {
   }
   getNextAnimalPosition(posX, posY) {
     const options = {
-      [GO_LEFT]: () => [--posX, posY],
-      [GO_RIGHT]: () => [++posX, posY],
-      [GO_DOWN]: () => [posX, ++posY],
-      [GO_UP]: () => [posX, --posY],
+      [GO_LEFT]: () => [posX - SIZE, posY],
+      [GO_RIGHT]: () => [posX + SIZE, posY],
+      [GO_DOWN]: () => [posX, posY + SIZE],
+      [GO_UP]: () => [posX, posY - SIZE],
     };
     if (
       ([GO_LEFT, GO_RIGHT].some(
@@ -112,22 +130,67 @@ class Motion {
   }
 }
 
-function setup() {
-  createCanvas(600, 600);
-}
-let i = 50;
+class Food {
+  constructor() {
+    if (typeof Food.instace !== "object") {
+      return Food.instace;
+    }
+    Food.instace = this;
+    this._posX = undefined;
+    this._posY = undefined;
+  }
 
-const snake = new Snake();
-const snakeMotion = new Motion();
+  getCurrentPos() {
+    return [this._posX, this._posY];
+  }
+
+  generate() {
+    const cols = Math.floor(CANVAS_WIDTH / SIZE);
+    const rows = Math.floor(CANVAS_HEIGHT / SIZE);
+    const posX = parseInt(Math.random() * SIZE) * cols;
+    const posY = parseInt(Math.random() * SIZE) * rows;
+    console.log(posX, posY);
+    this._posX = posX;
+    this._posY = posY;
+  }
+
+  draw() {
+    fill(200, 0, 0);
+    rect(this._posX, this._posY, 10, 10);
+  }
+}
+let snake = undefined;
+let snakeMotion = undefined;
+let apple = undefined;
+let initialSpeed = 5;
+let increased = false;
+function setup() {
+  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  snake = new Snake();
+  snakeMotion = new Motion();
+  apple = new Food();
+  apple.generate();
+  frameRate(initialSpeed);
+}
+
 function draw() {
   background(255);
   snake.moveAnimal(snakeMotion);
-  //snake.updateSnake(i++, snakeHead._posY);
+  snake.draw();
+  apple.draw();
+  snake.hasHitApple(apple);
+  if (!increased && snake.length % 5 === 0) {
+    increased = true;
+    initialSpeed++;
+    console.log("new speed", initialSpeed);
+    frameRate(initialSpeed);
+  } else if (snake.length % 5 !== 0) {
+    increased = false;
+  }
 }
 
 function keyPressed() {
   if (keyCode === UP_ARROW) {
-    debugger;
     snakeMotion.changeDirection(GO_UP);
   }
   if (keyCode === DOWN_ARROW) {
